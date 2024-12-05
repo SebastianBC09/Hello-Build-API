988c2f85e1647cb8e7e7515569ce66d13f161a3f
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2024-12-05 11:45:47 -0500

    feat: add routes and server configuration
    - Add dotenv configuration
    - Mount auth and github routes
    - Configure server port and listener

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const authRoutes = require('./routes/auth');
+const githubRoutes = require('./routes/github');
 const app = express();
-const port = 4000;
 
-app.get('/', (response, request) => {
-  response.send('Hello, World!')
-})
+dotenv.config();
 
-app.listen(port, () => {
-  console.log(`Hello World app listening at port ${4000}`);
-})
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/github', githubRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
