5e6e6ffc2301e6dcd0945dad05fc732f9e5f24d8
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2024-12-05 23:35:24 -0500

    feat: expand Auth0 configuration
    - Add Auth0 URL configurations
    - Add client and issuer settings
    - Configure redirect handling

--- src/config/auth0.js
+++ src/config/auth0.js
@@ -7,4 +7,13 @@ const jwtCheck = auth({
   tokenSigningAlg: 'RS256'
 });
 
-module.exports = { jwtCheck, checkScopes };
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    issuerBaseURL: process.env.AUTH0_DOMAIN,
+    secret: process.env.SECRET
+};
+
+module.exports = { jwtCheck, checkScopes, config };
