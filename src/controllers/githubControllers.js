71ac6cb5371eb30d3852a11850a9f339c2e27715
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2024-12-06 00:06:15 -0500

    feat: expand GitHub controller functionality
    - Add repository listing functionality
    - Add search implementation
    - Improve error handling
    - Add token validation

--- src/controllers/githubControllers.js
+++ src/controllers/githubControllers.js
@@ -1,4 +1,35 @@
 const favoritesService = require('../services/favoritesService');
+const { GraphQLClient } = require('graphql-request');
+const { GITHUB_API_URL } = require('../config/github');
+
+const listRepositories = async (request, response, next) => {
+  try {
+    const token = request.auth.token;
+    //TODO: Implement GraphQL query for repositories
+    const client = new GraphQLClient(GITHUB_API_URL, {
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    });
+    //TODO: Query implementation needed
+    response.status(200).json({ repositories: [] });
+  } catch (error) {
+    next(error);
+  }
+};
+const searchRepositories = async (request, response, next) => {
+  try {
+    const { query } = request.query;
+    const token = request.auth.token;
+    if (!query) {
+      return response.status(400).json({ message: 'Search query is required' });
+    }
+    //TODO: Implement search query
+    response.status(200).json({ repositories: [] });
+  } catch (error) {
+    next(error);
+  }
+};
 
 const getUserFavorites = (request, response, next) => {
   try {
@@ -9,7 +40,6 @@ const getUserFavorites = (request, response, next) => {
     next(error);
   }
 };
-
 const addToFavorites = (request, response, next) => {
   try {
     const userId = request.auth.sub;
@@ -20,7 +50,6 @@ const addToFavorites = (request, response, next) => {
     next(error);
   }
 };
-
 const removeFromFavorites = (request, response, next) => {
   try {
     const userId = request.auth.sub;
