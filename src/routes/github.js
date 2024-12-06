3f554b0dd4cd25ad647b22967fb8a93c91c6ea39
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2024-12-06 00:10:13 -0500

    refactor: update GitHub routes to use controllers
    - Replace placeholder responses with controller methods
    - Add listRepositories and searchRepositories routes
    - Maintain consistent middleware usage

--- src/routes/github.js
+++ src/routes/github.js
@@ -1,28 +1,16 @@
 const { Router } = require('express');
 const { jwtCheck } = require('../config/auth0');
-const { getUserFavorites, addToFavorites, removeFromFavorites } = require('../controllers/githubController');
+const {
+  getUserFavorites,
+  addToFavorites,
+  removeFromFavorites,
+  listRepositories,
+  searchRepositories
+} = require('../controllers/githubController');
 const router = Router();
 
-router.get('/repositories', jwtCheck, (request, response, next) => {
-  try {
-    response.status(200).json({
-      message: 'This protected route should get the repositories from the user'
-    });
-  } catch(error) {
-    next(error);
-  }
-});
-
-router.get('/search', jwtCheck, (request, response, next) => {
-  try {
-    response.status(200).json({
-      message: 'This protected route should get the results from the search'
-    });
-  } catch (error) {
-    next(error);
-  }
-});
-
+router.get('/repositories', jwtCheck, listRepositories);
+router.get('/search', jwtCheck, searchRepositories);
 router.get('/favorites', jwtCheck, getUserFavorites);
 router.post('/favorites/:repoId', jwtCheck, addToFavorites);
 router.delete('/favorites/:repoId', jwtCheck, removeFromFavorites);
