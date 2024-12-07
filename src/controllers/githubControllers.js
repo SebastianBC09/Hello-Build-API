8e3ead8b49f7badc87dfdbef0ed0a3a3c714fe25
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2024-12-06 20:33:43 -0500

    feat: TODOs completed, query implementation added

--- src/controllers/githubControllers.js
+++ src/controllers/githubControllers.js
@@ -1,23 +1,27 @@
 const favoritesService = require('../services/favoriteService');
 const { GraphQLClient } = require('graphql-request');
 const { GITHUB_API_URL } = require('../config/github');
+const { GET_USER_REPOSITORIES, SEARCH_REPOSITORIES } = require('../graphql/repositories')
 
 const listRepositories = async (request, response, next) => {
+  let repositories = [];
   try {
     const token = request.auth.token;
-    //TODO: Implement GraphQL query for repositories
     const client = new GraphQLClient(GITHUB_API_URL, {
       headers: {
         authorization: `Bearer ${token}`,
       },
     });
     //TODO: Query implementation needed
-    response.status(200).json({ repositories: [] });
+    const data = await client.request(GET_USER_REPOSITORIES);
+    repositories = data.viewer.repositories.nodes;
+    response.status(200).json({ repositories });
   } catch (error) {
     next(error);
   }
 };
 const searchRepositories = async (request, response, next) => {
+  let repositories = [];
   try {
     const { query } = request.query;
     const token = request.auth.token;
@@ -25,7 +29,14 @@ const searchRepositories = async (request, response, next) => {
       return response.status(400).json({ message: 'Search query is required' });
     }
     //TODO: Implement search query
-    response.status(200).json({ repositories: [] });
+    const client = new GraphQLClient(GITHUB_API_URL, {
+      headers: {
+        authorization: `Bearer ${token}`
+      }
+    })
+    const data = await client.request(SEARCH_REPOSITORIES);
+    repositories = data.search.edges.map(edge => edge.node);
+    response.status(200).json({ repositories });
   } catch (error) {
     next(error);
   }
